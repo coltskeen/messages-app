0dbee7832a0b1fbdc59325fae0e31bb5bfbd5d5c
Colt Skeen <colton.skeen@gmail.com>
2022-07-16 21:49:48 -0700

    linked the controller to the service

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,30 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDTO } from './DTO/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    messagesService: MessagesService;
+
+    constructor() {
+        //DON'T DO THIS ON REAL APP, USE DEPENDECY INJECTION
+
+        this.messagesService = new MessagesService();
+    }
+
+
     @Get()
-    listMessage() {}
+    listMessage() {
+        return this.messagesService.findAll();
+    }
 
     @Post()
     createMessage(@Body() body: CreateMessageDTO) {
-        console.log(body);
+        return this.messagesService.create(body.content);
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string) {
-        console.log(id); 
+        return this.messagesService.findOne(id);
     }
 }
